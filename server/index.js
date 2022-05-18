a6d8a77de79aff1d87fa7701f33869a674a5eb8c
Your Name <you@example.com>
2022-05-18 15:05:40 -0600

    git commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
+const { application } = require('express');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/")))
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
-
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.css"))
+})
 
 const port = process.env.PORT || 4005;
 
